3084dc0078df31ec10e385c00982c01fe20d2c1d
Cristian Ferreri <clferreri94@hotmail.com>
2019-12-07 21:53:38 -0300

    Se agregaron rutas para la api (AJAX)

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,31 @@ Route::get('jugeteando', function () {
     return "todo genial";
 });
 
+//-----------------------//
+//////// GENERAL //////////
+//-----------------------//
+
+//Traigo las ciudades de un pais
 Route::post('citysOfCountry', 'CountryAndCity\CiudaController@getCiudades');
 
-// Route::post('updateProfile', 'Profile\ProfileController@cambiarDatosPersonales');
+//-------------------------------------------//
 
+//-----------------------//
+//////// Cliente //////////
+//-----------------------//
 
 //Creacion de usuario 
 Route::post('createClient', 'AjaxControllers\AdminControllers\ClienteController@createCliente');
+
+//Creacion de beneficiario
+Route::post('createBeneficiaryAccount', 'AjaxControllers\AdminControllers\BeneficiaryAccountController@createBeneficiaryAccount');
+
+//Traerme los beneficiarios del cliente
+Route::post('BeneficiaryAccount', 'AjaxControllers\AdminControllers\BeneficiaryAccountController@getBeneficiaryAccount');
+
+//-------------------------------------------//
+
+//Traigo todas las cuentas Beneficiarias de un cliente
+//////////////////////////////////////////////////////////////////////////
+// Route::post('updateProfile', 'Profile\ProfileController@cambiarDatosPersonales');
+
